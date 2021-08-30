9a6ed6d78178a0683cca21dfb2077643b9184199
Oscar Castillo <manuel.rayo3@gmail.com>
2021-08-29 19:22:34 -0600

    New applications and menu.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 
 int main() {
-    int nota;
-    std::cout << "Digite la nota del curso: ";
-    std::cin >> nota;
-    if (nota >= 65) {
-        std::cout << "Felicidades, estimado estudiante. Usted ha aprobado el curso.";
-    }
-    else {
-        std::cout << "Salado papillo. Siga participando. Mejor suerte la proxima vez.";
-    }
+    menu();
     return 0;
 }
